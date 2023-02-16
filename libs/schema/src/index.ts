2ff66c0536caf443505b55e00168403aa58d1f6e
Andrew Zolotukhin <andrewzolotukhin@gmail.com>
2023-02-16 06:21:21 +0200

    Added default export to schema package

--- libs/schema/src/index.ts
+++ libs/schema/src/index.ts
@@ -1,9 +1,10 @@
-import { object } from './builders/ObjectSchemaBuilder.js';
-import { number } from './builders/NumberSchemaBuilder.js';
-import { InferType } from './builders/SchemaBuilder.js';
+import { any, AnySchemaBuilder } from './builders/AnySchemaBuilder.js';
+import { object, ObjectSchemaBuilder } from './builders/ObjectSchemaBuilder.js';
+import { number, NumberSchemaBuilder } from './builders/NumberSchemaBuilder.js';
+import { union, UnionSchemaBuilder } from './builders/UnionSchemaBuilder.js';
+import { InferType, SchemaBuilder } from './builders/SchemaBuilder.js';
 
 export {
-    SchemaBuilder,
     InferType,
     MakeOptional,
     MakeRequired,
@@ -14,6 +15,20 @@ export { any, AnySchemaBuilder } from './builders/AnySchemaBuilder.js';
 export { number, NumberSchemaBuilder } from './builders/NumberSchemaBuilder.js';
 export { object, ObjectSchemaBuilder } from './builders/ObjectSchemaBuilder.js';
 export { union, UnionSchemaBuilder } from './builders/UnionSchemaBuilder.js';
+
+export default {
+    any,
+    number,
+    object,
+    union,
+    builders: {
+        AnySchemaBuilder,
+        ObjectSchemaBuilder,
+        NumberSchemaBuilder,
+        UnionSchemaBuilder,
+        SchemaBuilder
+    }
+};
 
 /**
  * another comment here
